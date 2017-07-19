4e7ecb085cefe8804f09f86c979ffa351bac6317
Amanda Koster <amandakoster@gmail.com>
2017-07-19 14:59:39 -0700

    more scaffole

--- lab28-29-amanda/src/main.js
+++ lab28-29-amanda/src/main.js
@@ -1,1 +1,39 @@
 import './style/main.scss'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import AboutContainer from './component/about-container'
+import DashboardContainer from './component/dashboard-container'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      notes: [],
+    }
+
+    this.getApp = this.getApp.bind(this)
+  }
+  componentDidUpdate(){
+    console.log('::::STATE::::')
+  }
+
+  getApp(){
+    return{
+      state: this.state,
+      setState: this.setState.bind(this),
+    }
+  }
+
+  render(){
+    return(
+      <main>
+        <div>
+          <p> testing APP render </p>
+        </div>
+      </main>
+    )
+  }
+}
