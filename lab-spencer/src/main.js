3b56ad6a690663e9190c5da6b7746354c8455186
Spencer Gietzen <Speng1516@comcast.net>
2017-07-19 15:37:33 -0700

    main app setup, added webpack config

---
+++ lab-spencer/src/main.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import './style/_reset.scss';
+import './style/main.scss';
+import './style/_vars.scss';
+import './style/_base.scss';
+import './style/_layout.scss';
+
+import NoteCreateForm from './components/note-create-form';
+import NoteItem from './components/note-item';
+import NoteList from './components/note-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allNotes = null;
+    };
+
+    this.getApp = this.getApp.bind(this);
+  }
+
+  getApp() {
+    return {
+      state: this.state,
+      setState: this.setState.bind(this),
+    };
+  }
+
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
+  }
+
+  render() {
+    return (
+      <main>
+        <NoteCreateForm getApp=${this.getApp} />
+        <NoteList getApp=${this.getApp} />
+      </main>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
