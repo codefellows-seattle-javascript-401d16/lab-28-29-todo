2a38d20e4699d5194288ef28218eb4d18e6346cc
Justin Green <justin98366@gmail.com>
2017-07-20 12:14:13 -0700

    lab 28

---
+++ lab-justin/src/component/note-item/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class NoteItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(e){
+    e.preventDefault();
+    // this.props.notes.;
+    console.log('e', e.target.parentElement.value);
+    let array = this.props.notes;
+    array.splice(e.target.parentElement.value, 1);
+    this.props.app.setState(state => ({
+      notes: array,
+    }));
+  }
+
+  render() {
+
+    return (
+
+      <ul>
+        {this.props.notes.map((item, i) =>
+          <li key={i} value={i}>
+            {item.content}
+            <button onClick={this.handleDelete}>delete</button>
+          </li>
+        )}
+      </ul>
+    );
+  }
+}
+
+export default NoteItem;
