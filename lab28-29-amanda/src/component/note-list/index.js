db1f74ff924f84c212fe6cdb44761773c68c72d2
Amanda Koster <amandakoster@gmail.com>
2017-07-21 17:08:22 -0700

    delete and update work

--- lab28-29-amanda/src/component/note-list/index.js
+++ lab28-29-amanda/src/component/note-list/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NoteItem from '../note-item';
+import NoteForm from '../note-form';
 
 class NoteList extends React.Component {
   constructor(props){
@@ -12,7 +13,21 @@ class NoteList extends React.Component {
         <ul>
           {this.props.notes.map((item, i) =>
             <li key={i}>
+
               <h1>{item.title}</h1>
+
+              <button onClick={() => this.props.noteRemove(item)}>
+              x
+              </button>
+
+              <NoteForm
+                note={item}
+                submitTitle='update note'
+                handleSubmit={(note) => {
+                  note.id=item.id;
+                  this.props.noteUpdate(note);
+                }} />
+
             </li>
           )}
         </ul>
