e97ef7d611b98c720551f1a76cacfa8359077ca4
mikeporter <porter.michael.james@gmail.com>
2017-07-28 19:37:12 -0700

    working on testing

--- lab-mike/src/__test__/note-create-form.test.js
+++ lab-mike/src/__test__/note-create-form.test.js
@@ -7,4 +7,18 @@ describe('testing NoteCreateForm', () => {
     let wrapper = shallow(<NoteCreateForm handleNoteCreate={() => {}} />)
     expect(wrapper.state('inputText')).toBe('');
   });
+  test('expect submit to invoke handleNoteCreate', () => {
+    let noteCreate = (note) => {
+      expect(note.inputText).toEqual('');
+    }
+    let wrapper = shallow(<NoteCreateForm handleNoteCreate={noteCreate} />)
+    wrapper.find('button').simulate('click');
+  });
+  test('testing inputText change onChange handler', () => {
+    let noteCreate = (note) => {
+      expect(note.inputText).toEqual('1234');
+    }
+    let wrapper = shallow(<NoteCreateForm handleNoteCreate={noteCreate} />)
+    wrapper.find('input').simulate('change', {target: {name: 'inputText', value: '123'}});
+  });
 });
